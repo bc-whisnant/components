8cb977dbd217c05be450ff42ad0f0881a8a5a41b
Brandon Whisnant <brandonwhisnant@Testingmac--Brandon.local>
2020-04-08 21:07:43 -0400

    cleaning up

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-text-field></v-text-field>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'BaseInput',
+  }
+</script>
